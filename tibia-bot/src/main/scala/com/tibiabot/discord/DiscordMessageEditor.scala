4be3336e27f901220f39ef70d2764cfc062bb71b
Leo32 <bwils120@hotmail.com>
2023-04-18 01:00:52 +1000

    removing manual ratelimit, letting JDA handle it

--- tibia-bot/src/main/scala/com/tibiabot/discord/DiscordMessageEditor.scala
+++ tibia-bot/src/main/scala/com/tibiabot/discord/DiscordMessageEditor.scala
@@ -18,8 +18,6 @@ class DiscordMessageEditor() extends StrictLogging {
 
   private case class EmbedDetails(guild: Guild, deathChannel: TextChannel, messageId: String, timeStamp: ZonedDateTime)
   private val messages: ListBuffer[EmbedDetails] = ListBuffer.empty[EmbedDetails]
-  private val channelRateLimiters: mutable.Map[TextChannel, RateLimiter] = mutable.Map.empty
-  private val channelRateLimits: mutable.Map[TextChannel, (Int, Long)] = mutable.Map.empty
 
   val scheduler: ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
   scheduler.scheduleAtFixedRate(() => editMessages(), 0, 30, TimeUnit.SECONDS)
@@ -37,30 +35,6 @@ class DiscordMessageEditor() extends StrictLogging {
     if (!messages.isEmpty) {
       for (messageDetails <- messages) {
         if (messageDetails.timeStamp.plusMinutes(15).isBefore(ZonedDateTime.now())) {
-
-          val (count, lastUpdated) = channelRateLimits.getOrElse(messageDetails.deathChannel, (0, System.currentTimeMillis()))
-          if (System.currentTimeMillis() - lastUpdated < TimeUnit.MINUTES.toMillis(1)) {
-            if (count >= 20) {
-              // if more than 40 messages have been sent in the last minute, set the rate limiter to 1 per 3 seconds
-              val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.deathChannel, RateLimiter.create(1.0/3))
-              if (currentRate.getRate != 1.0/3){
-                channelRateLimiters.put(messageDetails.deathChannel, RateLimiter.create(1.0/3))
-                logger.warn(s"Rate limit for the death edits on Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.deathChannel.getId}' has been temporarily restricted to 1 per 3 seconds")
-              }
-            } else {
-              channelRateLimits.put(messageDetails.deathChannel, (count + 1, lastUpdated)) // increment count, but keep lastUpdated the same
-            }
-          } else {
-            val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.deathChannel, RateLimiter.create(1))
-            if (currentRate.getRate != 1){
-              channelRateLimiters.put(messageDetails.deathChannel, RateLimiter.create(1))
-            }
-            channelRateLimits.put(messageDetails.deathChannel, (1, System.currentTimeMillis())) // reset count and lastUpdated
-          }
-
-          // Acquire a permit from the rate limiter before sending the message
-          val rateLimiter = channelRateLimiters.getOrElseUpdate(messageDetails.deathChannel, RateLimiter.create(1))
-          rateLimiter.acquire()
 
           // Do the things
           try {
