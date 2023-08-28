ca5f58bb7612dd27c9529c3e3a2f170822bb3c25
Leo32 <bwils120@hotmail.com>
2023-08-29 08:17:08 +1000

    more level queue changes

--- tibia-bot/src/main/scala/com/tibiabot/discord/DiscordMessageSender.scala
+++ tibia-bot/src/main/scala/com/tibiabot/discord/DiscordMessageSender.scala
@@ -11,120 +11,116 @@ import net.dv8tion.jda.api.entities.{Guild, Webhook}
 import java.util.concurrent._
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
-import scala.jdk.CollectionConverters._
-import scala.util.{Failure, Success, Try}
-import java.util.concurrent.Executors
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Try, Success, Failure}
 
-//noinspection UnstableApiUsage
 class DiscordMessageSender() extends StrictLogging {
 
-  private case class MessageDetails(guild: Guild, webhookChannel: TextChannel, messageContent: String, messageAuthor: String)
+  private case class MessageDetails(webhookChannel: TextChannel, messageContent: String, messageAuthor: String)
 
-  private case class GuildMessageQueue(guild: Guild, queue: BlockingQueue[MessageDetails])
-
-  private val guildQueues: mutable.Map[Guild, GuildMessageQueue] = mutable.Map.empty
+  private val guildQueues: mutable.Map[Guild, mutable.Queue[MessageDetails]] = mutable.Map.empty
   private val channelRateLimiters: mutable.Map[TextChannel, RateLimiter] = mutable.Map.empty
   private val webhookRateLimits: mutable.Map[TextChannel, (Int, Long)] = mutable.Map.empty
-  private val guildProcessingThreads: mutable.Map[Guild, Thread] = mutable.Map.empty
 
-  // Create a thread pool with a fixed number of threads
-  private val threadPool = Executors.newFixedThreadPool(10) // Choose an appropriate number of threads
+  private implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
   def sendWebhookMessage(guild: Guild, webhookChannel: TextChannel, messageContent: String, messageAuthor: String): Unit = {
-    val messageDetails = MessageDetails(guild, webhookChannel, messageContent, messageAuthor)
-    try {
-      val guildQueue = guildQueues.getOrElseUpdate(guild, {
-        val newQueue = new LinkedBlockingQueue[MessageDetails]()
-        val guildMessageQueue = GuildMessageQueue(guild, newQueue)
-        // Start processing the new queue in the thread pool if not already started
-        if (!guildProcessingThreads.contains(guild)) {
-          threadPool.submit(() => processGuildQueue(guildMessageQueue))
-          guildProcessingThreads(guild) = true
-        }
-        guildMessageQueue
-      })
-      guildQueue.queue.put(messageDetails)
-    } catch {
-      case ex: Exception =>
-        logger.error(s"Failed to add level message to queue for Guild: '${guild.getId}' Channel: '${webhookChannel.getId}' World: '$messageAuthor':\nMessage: $messageContent", ex)
+    val messageDetails = MessageDetails(webhookChannel, messageContent, messageAuthor)
+    guildQueues.synchronized {
+      guildQueues.getOrElseUpdate(guild, mutable.Queue.empty[MessageDetails]).enqueue(messageDetails)
     }
   }
 
-  private def processGuildQueue(guildQueue: GuildMessageQueue): Unit = {
-    while (true) { // This loop keeps the thread alive for processing messages continuously
-      sendGuildMessages(guildQueue)
-      Thread.sleep(30000) // Wait for 30 seconds before processing the next batch
-    }
-  }
-
-  private def sendGuildMessages(guildQueue: GuildMessageQueue): Unit = {
-    val messages: ListBuffer[MessageDetails] = ListBuffer.empty[MessageDetails]
-    guildQueue.queue.drainTo(messages.asJava)
-    if (!messages.isEmpty) {
-      for (messageDetails <- messages) {
-
-        val (count, lastUpdated) = webhookRateLimits.getOrElse(messageDetails.webhookChannel, (0, System.currentTimeMillis()))
-        if (System.currentTimeMillis() - lastUpdated < TimeUnit.MINUTES.toMillis(1)) {
-          if (count >= 20) {
-            // if more than 40 messages have been sent in the last minute, set the rate limiter to 1 per 3 seconds
-            val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1.0/3))
-            if (currentRate.getRate != 1.0/3) {
-              channelRateLimiters.put(messageDetails.webhookChannel, RateLimiter.create(1.0/3))
-              logger.warn(s"Webhook rate limit for the levels channel on Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}' has been temporarily restricted to 1 per 3 seconds")
-            }
-          } else {
-            webhookRateLimits.put(messageDetails.webhookChannel, (count + 1, lastUpdated)) // increment count, but keep lastUpdated the same
+  private def sendMessagesForGuild(guild: Guild, messages: ListBuffer[MessageDetails]): Unit = {
+    for (messageDetails <- messages) {
+      val (count, lastUpdated) = webhookRateLimits.getOrElse(messageDetails.webhookChannel, (0, System.currentTimeMillis()))
+      if (System.currentTimeMillis() - lastUpdated < TimeUnit.MINUTES.toMillis(1)) {
+        if (count >= 20) {
+          // if more than 20 messages have been sent in the last minute, set the rate limiter to 1 per 3 seconds
+          val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1.0 / 3))
+          if (currentRate.getRate != 1.0 / 3) {
+            channelRateLimiters.put(messageDetails.webhookChannel, RateLimiter.create(1.0 / 3))
+            logger.warn(
+              s"Webhook rate limit for the levels channel on Guild: '${guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}' has been temporarily restricted to 1 per 3 seconds"
+            )
           }
         } else {
-          val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1))
-          if (currentRate.getRate != 1) {
-            channelRateLimiters.put(messageDetails.webhookChannel, RateLimiter.create(1))
-          }
-          webhookRateLimits.put(messageDetails.webhookChannel, (1, System.currentTimeMillis())) // reset count and lastUpdated
+          webhookRateLimits.put(messageDetails.webhookChannel, (count + 1, lastUpdated)) // increment count, but keep lastUpdated the same
         }
+      } else {
+        val currentRate = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1))
+        if (currentRate.getRate != 1) {
+          channelRateLimiters.put(messageDetails.webhookChannel, RateLimiter.create(1))
+        }
+        webhookRateLimits.put(messageDetails.webhookChannel, (1, System.currentTimeMillis())) // reset count and lastUpdated
+      }
 
-        // Acquire a permit from the rate limiter before sending the message
-        val rateLimiter = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1))
-        rateLimiter.acquire()
+      // Acquire a permit from the rate limiter before sending the message
+      val rateLimiter = channelRateLimiters.getOrElseUpdate(messageDetails.webhookChannel, RateLimiter.create(1))
+      rateLimiter.acquire()
 
-        var webhookCheck = true
-        val getWebHook = Try(messageDetails.webhookChannel.retrieveWebhooks().submit().get()) match {
-          case Success(webhooks) => webhooks
-          case Failure(_) =>
-            webhookCheck = false
-            List.empty[Webhook].asJava
+      var webhook: Webhook = null
+      var webhookCheck = true
+      Try(messageDetails.webhookChannel.retrieveWebhooks().complete()) match {
+        case Success(webhooks) if !webhooks.isEmpty =>
+          webhook = webhooks.get(0)
+        case _ =>
+          webhookCheck = false
+      }
+
+      if (webhookCheck) {
+        if (webhook == null) {
+          try {
+            val createWebhook = messageDetails.webhookChannel.createWebhook(messageDetails.messageAuthor).complete()
+            webhook = createWebhook
+          } catch {
+            case ex: Exception =>
+              logger.warn(
+                s"Failed to CREATE webhook for Guild: '${guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'",
+                ex
+              )
+          }
         }
-        var webhook: Webhook = null
-        if (getWebHook.isEmpty && webhookCheck) {
-          try {
-            val createWebhook = messageDetails.webhookChannel.createWebhook(messageDetails.messageAuthor).submit()
-            webhook = createWebhook.get()
-          } catch {
-            case ex: Exception => logger.warn(s"Failed to CREATE webhook for Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'", ex)
-          }
-        } else if (!getWebHook.isEmpty && webhookCheck) {
-          try {
-            webhook = getWebHook.get(0)
-          } catch {
-            case ex: Exception => logger.warn(s"Failed to GET webhook for Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'", ex)
-          }
-        } else {
-          logger.warn(s"Failed to RETRIEVE webhooks for Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'")
-        }
-        if (webhook != null) {
-          val webhookUrl = webhook.getUrl
-          val messageBuilder = new WebhookMessageBuilder()
-            .setUsername(messageDetails.messageAuthor)
-            .setContent(messageDetails.messageContent)
-            .setAvatarUrl(Config.webHookAvatar)
-          val client = new WebhookClientBuilder(webhookUrl).build()
-          try {
-            client.send(messageBuilder.build())
-          } catch {
-            case ex: Exception => logger.error(s"Failed to SEND webhook for Guild: '${messageDetails.guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'", ex)
-          }
+      }
+
+      if (webhook != null) {
+        val webhookUrl = webhook.getUrl
+        val messageBuilder = new WebhookMessageBuilder()
+          .setUsername(messageDetails.messageAuthor)
+          .setContent(messageDetails.messageContent)
+          .setAvatarUrl(Config.webHookAvatar)
+        val client = new WebhookClientBuilder(webhookUrl).build()
+        try {
+          client.send(messageBuilder.build())
+        } catch {
+          case ex: Exception =>
+            logger.error(
+              s"Failed to SEND webhook for Guild: '${guild.getId}' Channel: '${messageDetails.webhookChannel.getId}'  World: '${messageDetails.messageAuthor}'",
+              ex
+            )
+        } finally {
           client.close()
         }
+      }
+    }
+  }
+
+  private def sendMessages(): Unit = {
+    val guildMessagePairs: List[(Guild, ListBuffer[MessageDetails])] = guildQueues.synchronized {
+      guildQueues.map { case (guild, queue) =>
+        val messages = ListBuffer.empty[MessageDetails]
+        queue.synchronized {
+          while (queue.nonEmpty) {
+            messages += queue.dequeue()
+          }
+        }
+        (guild, messages)
+      }.toList
+    }
+
+    Future.traverse(guildMessagePairs) { case (guild, messages) =>
+      Future {
+        sendMessagesForGuild(guild, messages)
       }
     }
   }
