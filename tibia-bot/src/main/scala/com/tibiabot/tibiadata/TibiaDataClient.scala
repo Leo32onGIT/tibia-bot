7e10b888612f25ed8f2f715236741dbe6ab3d031
Leo32 <bwils120@hotmail.com>
2024-04-20 17:25:39 +1000

    Axera changes

--- tibia-bot/src/main/scala/com/tibiabot/tibiadata/TibiaDataClient.scala
+++ tibia-bot/src/main/scala/com/tibiabot/tibiadata/TibiaDataClient.scala
@@ -176,7 +176,7 @@ class TibiaDataClient extends JsonSupport with StrictLogging {
     val level = input._2
     val world = input._3
     val encodedName = URLEncoder.encode(name, "UTF-8").replaceAll("\\+", "%20")
-    val bypassName: String = if ((world == "Pulsera" && level >= 400) || (world == "Runera" && level >= 200) || (world == "Victoris" && level >= 80)) {
+    val bypassName: String = if ((world == "Pulsera" && level >= 400) || (world == "Axera" && level >= 400) || (world == "Victoris" && level >= 80)) {
       val random = new Random()
       // Append randomly generated "+" characters to the last word, limited to a maximum length of 20
       val numPluses = math.min(random.nextInt(7), 20 - encodedName.length) // Randomly generate 0-6 "+" characters, limited to a max length of 20
