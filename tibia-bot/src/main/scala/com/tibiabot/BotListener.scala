cbf7892910f54b1862642a1121f69ec6f98dbe4d
Leo32 <bwils120@hotmail.com>
2024-04-28 03:53:00 +1000

    Update BotListener.scala

--- tibia-bot/src/main/scala/com/tibiabot/BotListener.scala
+++ tibia-bot/src/main/scala/com/tibiabot/BotListener.scala
@@ -695,7 +695,7 @@ class BotListener extends ListenerAdapter with StrictLogging {
               } else {
 
                 // default emoji regex
-                val emojiPattern = """/<a?:.+?:\d{18}>|\\p{Extended_Pictographic}/"""
+                val emojiPattern = "/<a?:.+?:\\d{18}>|\\p{Extended_Pictographic}/gu"
                 val matched = emojiOption.matches(emojiPattern)
                 if (matched) {
                   BotApp.addOnlineListCategory(event, typeOption, nameOption, labelOption, emojiOption, embed => {
