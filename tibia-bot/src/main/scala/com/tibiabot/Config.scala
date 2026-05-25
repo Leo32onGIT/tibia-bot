f70f23f97d861519b76c53be138ef210f9c1e3f5
Violent Beams <bwils120@hotmail.com>
2026-05-26 04:35:08 +1000

    Revert "support medal and twitch clips in death screenshots"
    This reverts commit 4f25b733ee8a3679de41bbf09d2984ac37ee86f5.

--- tibia-bot/src/main/scala/com/tibiabot/Config.scala
+++ tibia-bot/src/main/scala/com/tibiabot/Config.scala
@@ -29,7 +29,7 @@ object Config {
     "**Commands & Features:**\n" +
     "All interactions with the bot are done through **[slash commands](https://support.discord.com/hc/en-us/articles/1500000368501-Slash-Commands-FAQ)**.\n" +
     "If you type `/` and click on **Violent Bot** - you will see all the commands available to you.\n\n" +
-    "[Website](https://violentbot.xyz) | [Discord](https://discord.gg/SWMq9Pz8ud) | [Patreon](https://patreon.com/violentbot)"
+    "[Website](https://violentbot.xyz) | [Discord](https://discord.gg/SWMq9Pz8ud) | [Donate](http://donate.violentbot.xyz)"
 
   // discord config
   val webHookAvatar: String = discord.getString("avatar-url")
